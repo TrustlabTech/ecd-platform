4e062590b03327d9dc51b9f6a6d14818b5d19aa6
donnierisk <donnierisk@gmail.com>
2018-03-19 15:47:14 +0200

    fetchById for staff

---
+++ app/Http/Requests/Staff/FetchByIDRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Staff;
+
+use App\Http\Requests\Request;
+
+class FetchByIDRequest extends Request
+{
+
+    protected $redirectRoute = 'staff.create';
+
+    /**
+    * Get the validation rules that apply to the request.
+    *
+    * @return array
+    */
+    public function rules()
+    {
+        return [
+            'id_number' => 'required|id_valid'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'id_number.required' => 'The ID number is required.',
+            'id_number.id_valid' => 'The ID number used is not valid.',
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+}
