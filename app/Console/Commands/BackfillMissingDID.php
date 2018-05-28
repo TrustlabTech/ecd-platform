5879dff4d6c04dc9645a0af3075550b6a69a986b
Zayin Krige <zkrige@users.noreply.github.com>
2018-05-28 09:41:48 +0200

    Backfill missing DID's (#8)
    * add: filter all children by ecd center
    
    * fix: spelling mistake
    
    * fix: add missing route
    
    * Adding command to re-submit any records with a null did field.
    Run `php artisan backfill-did` to see the total numbers missing.
    Run `php artisan backfill-did --confirm` to actually post them.

---
+++ app/Console/Commands/BackfillMissingDID.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTFactory;
+
+use App\Models\Child;
+use App\Models\Staff;
+use App\Models\Centre;
+
+class BackfillMissingDID extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backfill-did {--confirm}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fix all Staff, Children and Centers that have no DID';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info("Finding missing DIDs");
+
+        $this->info("[child] Missing: " . Child::whereNull('did')->count());
+        $this->info("[centre] Missing: " . Centre::whereNull('did')->count());
+        $this->info("[practitioner] Missing: " . Staff::whereNull('did')->count());
+
+        $this->info('');
+
+        if ($this->option('confirm'))
+        {
+            $this->info("--confirm switch on, starting sync.");
+
+            foreach(Child::whereNull('did')->get() as $res)
+                $this->post($res->id, 'child');
+
+            foreach(Centre::whereNull('did')->get() as $res)
+                $this->post($res->id, 'centre');
+
+            foreach(Staff::whereNull('did')->get() as $res)
+                $this->post($res->id, 'practitioner');
+        }
+        else
+        {
+            $this->info("--confirm not set, doing nothing");
+        }
+
+    }
+
+    private function post($resource_id, $resource_type)
+    {
+        $endpoint = env('API_V2_CREATE_DID_ENDPOINT', 'http://api.amply.tech/eis');
+        $payload = JWTFactory::sub(-1)->make(); // center
+        $token = JWTAuth::encode($payload);
+
+        $restClient = new GuzzleHttp\Client();
+
+        try {
+            $restClient->post($endpoint, [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer ' . $token
+                ],
+                'json' => [
+                    'id' => $resource_id,
+                    'type' => $resource_type
+                ]
+            ]);
+
+            $this->info("[$resource_type] ID $resource_id posted to EIS");
+
+        }
+        catch(\Exception $ex)
+        {
+            $this->error("[$resource_type] ID $resource_id could not be posted!");
+            dd($ex->getMessage());
+        }
+
+    }
+}
